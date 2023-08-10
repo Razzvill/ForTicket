6b72536ecff7cb25303317a12d475f086ac12ad0
Razzvill <141206282+Razzvill@users.noreply.github.com>
2023-08-10 17:55:46 +0900

    공연장 VO, DAO, Service 추가
    공연장 VO, DAO, Service 소스파일 추가

---
+++ forTicket/src/main/java/com/forTicket/theater/service/TheaterServiceImpl.java
@@ -0,0 +1,50 @@
+package com.forTicket.theater.service;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.forTicket.theater.dao.TheaterDAO;
+import com.forTicket.theater.vo.TheaterVO;
+
+@Service("memberService")
+@Transactional(propagation = Propagation.REQUIRED)
+public class TheaterServiceImpl implements TheaterService {
+	@Autowired
+	private TheaterDAO theaterDAO;
+	
+	@Override
+	public List listTheaters() throws DataAccessException {
+		List listTheaters = null;
+		listTheaters = theaterDAO.selectAllTheaterList();
+		return listTheaters;
+	}
+
+	@Override
+	public TheaterVO theaterInfo(int theater_id) throws DataAccessException {
+		TheaterVO theaterVO = new TheaterVO();
+		theaterVO = theaterDAO.selectTheaterInfo(theater_id);
+		return theaterVO;
+	}
+
+	@Override
+	public int addTheater(Map theaterMap) throws DataAccessException {
+		return theaterDAO.insertTheater(theaterMap);
+	}
+
+	@Override
+	public int modTheater(Map theaterMap) throws DataAccessException {
+		return theaterDAO.updateTheater(theaterMap);
+	}
+
+	@Override
+	public int removeTheater(Map theaterMap) throws DataAccessException {
+		return theaterDAO.deleteTheater(theaterMap);
+	}
+	
+}
