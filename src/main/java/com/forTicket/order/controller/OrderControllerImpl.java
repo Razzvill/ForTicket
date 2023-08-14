90f2eb830beb077f37bb36adc54b28b719bfe49f
Razzvill <ksc2562@gmail.com>
2023-08-14 15:22:33 +0900

    예매페이지(좌석지정, 미지정) 추가  - order_seat.jsp, order_seatNone.jsp 예매 컨트롤러(OrderController) 매핑 수정  - /member/order.do -> /member/order_seat.do  - /member/order_seatNone.do 추가

--- src/main/java/com/forTicket/order/controller/OrderControllerImpl.java
+++ src/main/java/com/forTicket/order/controller/OrderControllerImpl.java
@@ -14,14 +14,23 @@ import com.forTicket.goods.controller.GoodsController;
 @Controller("OrderController")
 public class OrderControllerImpl implements OrderController{
 
-	//주문페이지(날짜선택 등)
-	@RequestMapping(value={"/member/order.do"}, method={RequestMethod.GET, RequestMethod.POST})
-	private ModelAndView order(HttpServletRequest req, HttpServletResponse resp) throws Exception {
+	//예매 페이지-좌석선택(날짜선택 등)
+	@RequestMapping(value={"/member/order_seat.do"}, method={RequestMethod.GET, RequestMethod.POST})
+	private ModelAndView order_seat(HttpServletRequest req, HttpServletResponse resp) throws Exception {
 		String viewName = (String)req.getAttribute("viewName");
 		ModelAndView mav = new ModelAndView();
 		mav.setViewName(viewName);
 		return mav;
 	}
+	
+	//예매 페이지-좌석 미선택(날짜선택 등)
+		@RequestMapping(value={"/member/order_seatNone.do"}, method={RequestMethod.GET, RequestMethod.POST})
+		private ModelAndView order_seatNone(HttpServletRequest req, HttpServletResponse resp) throws Exception {
+			String viewName = (String)req.getAttribute("viewName");
+			ModelAndView mav = new ModelAndView();
+			mav.setViewName(viewName);
+			return mav;
+		}
 	
 	//결제화면
 	@RequestMapping(value= "/order/ticketReservation.do", method = {RequestMethod.GET,RequestMethod.POST})
