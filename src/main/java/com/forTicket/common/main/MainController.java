22a43d024b87e64e504d05c3d18166ccc95bda0a
SER009 <141905543+SER009@users.noreply.github.com>
2023-08-17 17:33:51 +0900

    1.메인 -엔터 2.모드멤터.jsp - 추가

--- src/main/java/com/forTicket/common/main/MainController.java
+++ src/main/java/com/forTicket/common/main/MainController.java
@@ -10,7 +10,30 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller("mainController")
 public class MainController {
-	 @RequestMapping(value={"/main.do"}, method={RequestMethod.GET,RequestMethod.POST})
+	 
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	@RequestMapping(value={"/main.do"}, method={RequestMethod.GET,RequestMethod.POST})
 	 private ModelAndView main(HttpServletRequest req,HttpServletResponse resp) throws Exception {
 		 String viewName = (String)req.getAttribute("viewName");
 		 ModelAndView mav = new ModelAndView();
